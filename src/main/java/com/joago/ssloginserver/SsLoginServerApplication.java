0ce05cd1ae0572e3187f5845f891c068fc2714b9
joagz <joagomez.dev@gmail.com>
2023-10-23 12:24:15 -0300

    first commit

---
+++ src/main/java/com/joago/ssloginserver/SsLoginServerApplication.java
@@ -0,0 +1,13 @@
+package com.joago.ssloginserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsLoginServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsLoginServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joago/ssloginserver/SsLoginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joago.ssloginserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsLoginServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
